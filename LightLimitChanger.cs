f665840b10bae9e92a9b8c7b50f771b5026b69a5
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2023-06-15 18:30:33 +0900

    🆗（2）

--- LightLimitChanger.cs
+++ LightLimitChanger.cs
@@ -13,7 +13,7 @@ using System.Linq;
 
 namespace io.github.azukimochi
 {
-    public class LightLimitChanger : EditorWindow
+    public sealed class LightLimitChanger : EditorWindow
     {
         public bool IsDefaultUse = false;
         public bool IsValueSave = false;
@@ -22,11 +22,11 @@ namespace io.github.azukimochi
         public float MinLightValue = 0.0f;
         public VRCAvatarDescriptor TargetAvatar;
 
-        const string SHADER_KEY_LightMinLimit = "_LightMinLimit";
-        const string SHADER_KEY_LightMaxLimit = "_LightMaxLimit";
-        const string ParameterName_Toggle = "LightLimitEnable";
-        const string ParameterName_Value = "LightLimitValue";
-        const string ArtifactsFolderGUID = "f60eb27a31a2bb640a2b06ae64950203";
+        private const string SHADER_KEY_LightMinLimit = "_LightMinLimit";
+        private const string SHADER_KEY_LightMaxLimit = "_LightMaxLimit";
+        private const string ParameterName_Toggle = "LightLimitEnable";
+        private const string ParameterName_Value = "LightLimitValue";
+        private const string ArtifactsFolderGUID = "f60eb27a31a2bb640a2b06ae64950203";
 
         private string infoLabel = "";
 
@@ -182,10 +182,6 @@ namespace io.github.azukimochi
             fx.AddParameter(new AnimatorControllerParameter() { name = ParameterName_Toggle, defaultBool = IsDefaultUse, type = AnimatorControllerParameterType.Bool });
             fx.AddParameter(new AnimatorControllerParameter() { name = ParameterName_Value, defaultFloat = DefaultLightValue, type = AnimatorControllerParameterType.Float });
 
-            //////////////////////
-            //ExpressionMenuの生成
-
-            //Menuの生成
             var menu = new VRCExpressionsMenu()
             {
                 controls = new List<VRCExpressionsMenu.Control>
@@ -239,206 +235,6 @@ namespace io.github.azukimochi
             var parameters = prefab.GetOrAddComponent<ModularAvatarParameters>();
             parameters.parameters.Add(new ParameterConfig() { nameOrPrefix = ParameterName_Toggle, saved = IsValueSave, defaultValue = IsDefaultUse ? 1 : 0, syncType = ParameterSyncType.Bool });
             parameters.parameters.Add(new ParameterConfig() { nameOrPrefix = ParameterName_Value, saved = IsValueSave, defaultValue = DefaultLightValue, syncType = ParameterSyncType.Float });
-
-        }
-
-        private void GenerateAssets(string savePath, string saveName, GameObject avater)
-        {
-            /////////////////////
-            //アニメーションの生成
-            var animLightChangeClip = new AnimationClip();
-            var animLightDisableClip = new AnimationClip();
-
-            //アニメーションをまとめて生成
-            createAnimationClip(avater, animLightChangeClip, animLightDisableClip, avater.name);
-            AssetDatabase.CreateAsset(animLightChangeClip, $"{savePath}_active.anim");
-            AssetDatabase.CreateAsset(animLightDisableClip, $"{savePath}_deactive.anim");
-
-            //////////////////////////////////
-            //アニメーターコントローラーの生成
-            var controller = AnimatorController.CreateAnimatorControllerAtPath($"{savePath}.controller");
-
-            controller.parameters = new AnimatorControllerParameter[]
-            {
-            new AnimatorControllerParameter()
-            {
-                name = saveName,
-                type = AnimatorControllerParameterType.Bool,
-                defaultBool = this.IsDefaultUse
-            },
-            new AnimatorControllerParameter()
-            {
-                name = saveName + "_motion",
-                type = AnimatorControllerParameterType.Float,
-                defaultFloat = this.DefaultLightValue
-            }
-            };
-            //レイヤーとステートの設定
-            var layer = controller.layers[0];
-            layer.name = saveName;
-            layer.stateMachine.name = saveName;
-
-            var lightActiveState = layer.stateMachine.AddState($"{saveName}_Active", new Vector3(300, 200));
-            lightActiveState.motion = animLightChangeClip;
-            lightActiveState.writeDefaultValues = false;
-            lightActiveState.timeParameterActive = true;
-            lightActiveState.timeParameter = saveName + "_motion";
-
-            var lightDeActiveState = layer.stateMachine.AddState($"{saveName}_DeActive", new Vector3(300, 100));
-            lightDeActiveState.motion = animLightDisableClip;
-            lightDeActiveState.writeDefaultValues = false;
-            layer.stateMachine.defaultState = lightDeActiveState;
-
-            var toDeActiveTransition = lightActiveState.AddTransition(lightDeActiveState);
-            toDeActiveTransition.exitTime = 0;
-            toDeActiveTransition.duration = 0;
-            toDeActiveTransition.hasExitTime = false;
-            toDeActiveTransition.conditions = new AnimatorCondition[] {
-                new AnimatorCondition
-                {
-                    mode = AnimatorConditionMode.IfNot,
-                    parameter = saveName,
-                    threshold = 1,
-                },
-            };
-            var toActiveTransition = lightDeActiveState.AddTransition(lightActiveState);
-            toActiveTransition.exitTime = 0;
-            toActiveTransition.duration = 0;
-            toActiveTransition.hasExitTime = false;
-            toActiveTransition.conditions = new AnimatorCondition[] {
-                new AnimatorCondition
-                {
-                    mode = AnimatorConditionMode.If,
-                    parameter = saveName,
-                    threshold = 1,
-                },
-            };
-
-            //////////////////////
-            //ExpressionMenuの生成
-
-            //SubMenuの生成
-            var subMenu = new VRCExpressionsMenu
-            {
-                controls = new List<VRCExpressionsMenu.Control>
-                {
-                new VRCExpressionsMenu.Control {
-                        name = saveName + " Toggle",
-                        type = VRCExpressionsMenu.Control.ControlType.Toggle,
-                        parameter = new VRCExpressionsMenu.Control.Parameter
-                        {
-                            name = saveName,
-                        },
-                        subParameters = new VRCExpressionsMenu.Control.Parameter[] { },
-                        value = 1,
-                        labels = new VRCExpressionsMenu.Control.Label[] { },
-                    },
-                new VRCExpressionsMenu.Control {
-                    name = saveName + " light",
-                    type = VRCExpressionsMenu.Control.ControlType.RadialPuppet,
-                    subParameters = new VRCExpressionsMenu.Control.Parameter[] {
-                        new VRCExpressionsMenu.Control.Parameter
-                        {
-                            name = saveName + "_motion"
-                        }
-                    },
-                    value = DefaultLightValue,
-                    labels = new VRCExpressionsMenu.Control.Label[] { },
-                },
-             },
-            };
-            AssetDatabase.CreateAsset(subMenu, $"{savePath}_subMenu.asset");
-
-            //Menuの生成
-            var menu = new VRCExpressionsMenu()
-            {
-                controls = new List<VRCExpressionsMenu.Control>
-                {
-                new VRCExpressionsMenu.Control {
-                        name = saveName,
-                        type = VRCExpressionsMenu.Control.ControlType.SubMenu,
-                        subMenu = subMenu,
-                        subParameters = new VRCExpressionsMenu.Control.Parameter[] { },
-                        labels = new VRCExpressionsMenu.Control.Label[] { },
-                    },
-             },
-            };
-            AssetDatabase.CreateAsset(menu, $"{savePath}.asset");
-
-            //////////////
-            //Prefabの生成
-
-            var prefab = new GameObject(saveName);
-            var menuInstaller = prefab.GetOrAddComponent<ModularAvatarMenuInstaller>();
-            menuInstaller.menuToAppend = menu;
-            var parameters = prefab.GetOrAddComponent<ModularAvatarParameters>();
-
-            //MA Parameters
-            parameters.parameters.Add(new ParameterConfig
-            {
-                nameOrPrefix = saveName,
-                defaultValue = (float)Convert.ToDouble(this.IsDefaultUse),
-                syncType = ParameterSyncType.Bool,
-                saved = this.IsValueSave
-            });
-            parameters.parameters.Add(new ParameterConfig
-            {
-                nameOrPrefix = name = saveName + "_motion",
-                defaultValue = this.DefaultLightValue,
-                syncType = ParameterSyncType.Float,
-                saved = this.IsValueSave
-            });
-            // MA MergeAnimator
-            var mergeAnimator = prefab.GetOrAddComponent<ModularAvatarMergeAnimator>();
-            mergeAnimator.animator = controller;
-            mergeAnimator.layerType = VRCAvatarDescriptor.AnimLayerType.FX;
-            mergeAnimator.pathMode = MergeAnimatorPathMode.Absolute;
-            mergeAnimator.matchAvatarWriteDefaults = true;
-            PrefabUtility.SaveAsPrefabAsset(prefab, $"{savePath}.prefab");
-            DestroyImmediate(prefab);
-
-            AssetDatabase.SaveAssets();
-
-            Debug.Log(savePath);
-            Debug.Log(saveName);
-        }
-        private void createAnimationClip(GameObject parentObj, AnimationClip animActiveClip, AnimationClip animDeActiveClip, string avaterName)
-        {
-            Transform children = parentObj.GetComponent<Transform>();
-
-            if (children.childCount == 0) return;
-
-            foreach (Transform obj in children)
-            {
-                SkinnedMeshRenderer SkinnedMeshR = obj.gameObject.GetComponent<SkinnedMeshRenderer>();
-                if (SkinnedMeshR != null)
-                {
-                    foreach (var mat in SkinnedMeshR.sharedMaterials)
-                    {
-                        if (mat != null && (mat.shader.name.IndexOf("lilToon") != -1 || mat.shader.name.IndexOf("motchiri") != -1))
-                        {
-                            string path = getObjectPath(obj).Replace(avaterName + "/", "");
-                            animActiveClip.SetCurve(path, typeof(SkinnedMeshRenderer), SHADER_KEY_LightMinLimit, new AnimationCurve(new Keyframe(0 / 60.0f, MinLightValue), new Keyframe(1 / 60.0f, MaxLightValue)));
-                            animActiveClip.SetCurve(path, typeof(SkinnedMeshRenderer), SHADER_KEY_LightMaxLimit, new AnimationCurve(new Keyframe(0 / 60.0f, MinLightValue), new Keyframe(1 / 60.0f, MaxLightValue)));
-
-                            animDeActiveClip.SetCurve(path, typeof(SkinnedMeshRenderer), SHADER_KEY_LightMinLimit, new AnimationCurve(new Keyframe(0 / 60.0f, 0.0f), new Keyframe(1 / 60.0f, 0.0f)));
-                            animDeActiveClip.SetCurve(path, typeof(SkinnedMeshRenderer), SHADER_KEY_LightMaxLimit, new AnimationCurve(new Keyframe(0 / 60.0f, 1.0f), new Keyframe(1 / 60.0f, 1.0f)));
-                        }
-                    }
-                }
-                createAnimationClip(obj.gameObject, animActiveClip, animDeActiveClip, avaterName);
-            }
-        }
-        private string getObjectPath(Transform trans)
-        {
-            string path = trans.name;
-            var parent = trans.parent;
-            while (parent)
-            {
-                path = $"{parent.name}/{path}";
-                parent = parent.parent;
-            }
-            return path;
         }
     }
 
@@ -455,19 +251,6 @@ namespace io.github.azukimochi
             obj.hideFlags |= HideFlags.HideInHierarchy;
             return obj;
         }
-
-        public static IEnumerable<string> EnumerateProperties(this Shader shader)
-        {
-            var count = shader.GetPropertyCount();
-            for(int i = 0; i < count; i++)
-            {
-                yield return shader.GetPropertyName(i);
-            }
-        }
-
-        public static string GetAbsolutePath(this GameObject obj) => GetRelativePath(obj.transform, null);
-
-        public static string GetRelativePath(this GameObject obj, GameObject root, bool includeRelativeTo = false) => obj.transform.GetRelativePath(root.transform, includeRelativeTo);
 
         public static string GetRelativePath(this Transform transform, Transform root, bool includeRelativeTo = false)
         {
