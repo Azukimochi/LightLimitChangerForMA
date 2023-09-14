7b253aa7d862f3a883fd4b6508cfeb41c07fec80
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2023-09-14 18:19:54 +0900

    fix `HasFlag`

--- Editor/Utils.cs
+++ Editor/Utils.cs
@@ -13,7 +13,7 @@ namespace io.github.azukimochi
 {
     internal static class Utils
     {
-        public static bool HasFlag(this int x, int y) => (x & y) != y;
+        public static bool HasFlag(this int x, int y) => (x & y) == y;
 
         public static void ClearSubAssets(this Object obj)
         {
