13bbbdb65fcee6a2c81bfc65db5b2cf8ce0c2eb7
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-08-18 16:37:34 +0900

    feat: Add component inspector

---
+++ Editor/Inspector/LightLimitChangerComponentEditor.cs
@@ -0,0 +1,12 @@
+﻿using Target = io.github.azukimochi.LightLimitChangerComponent;
+
+namespace io.github.azukimochi;
+
+[CustomEditor(typeof(Target))]
+internal sealed class LightLimitChangerComponentEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
