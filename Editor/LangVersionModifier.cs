cdfe47e14e942d98b9bd7f9d46d6e37a688a7879
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-08-18 17:04:11 +0900

    fix: LangVersionModifier

--- Editor/LangVersionModifier.cs
+++ Editor/LangVersionModifier.cs
@@ -11,23 +11,30 @@ namespace io.github.azukimochi
     {
         public static string OnGeneratedCSProject(string path, string content)
         {
-            if (!path.Contains("io.github.azukimochi.light-limit-changer", StringComparison.OrdinalIgnoreCase))
+            try
+            {
+                if (!path.Contains("io.github.azukimochi.light-limit-changer", StringComparison.OrdinalIgnoreCase))
+                    return content;
+
+                var xml = XDocument.Parse(content);
+                var langVersion = xml.Root.Elements("PropertyGroup")
+                    .SelectMany(x => x.Elements("LangVersion"))
+                    .FirstOrDefault();
+                if (langVersion is not null)
+                {
+                    langVersion.Value = "10";
+                }
+                else
+                {
+                    xml.Root.Element("PropertyGroup").Add(new XElement("LangVersion", "10"));
+                }
+
+                return xml.ToString();
+            }
+            catch
+            {
                 return content;
-
-            var xml = XDocument.Parse(content);
-            var langVersion = xml.Root.Elements("PropertyGroup")
-                .SelectMany(x => x.Elements("LangVersion"))
-                .FirstOrDefault();
-            if (langVersion is not null)
-            {
-                langVersion.Value = "10";
             }
-            else
-            {
-                xml.Root.Element("PropertyGroup").Add(new XElement("LangVersion", "10"));
-            }
-
-            return xml.ToString();
         }
     }
 }
