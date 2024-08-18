21103195ea2534ad2c1bf993ef504074ffe865ea
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-08-18 21:53:56 +0900

    feat: Add ParameterExt

---
+++ Editor/Utils/ParameterExt.cs
@@ -0,0 +1,24 @@
+﻿namespace io.github.azukimochi;
+
+internal static class ParameterExt
+{
+    public static Parameter<float> ToFloat<T>(this Parameter<T> parameter)
+    {
+        var result = new Parameter<float>()
+        {
+            Enable = parameter.Enable,
+            Saved = parameter.Saved,
+            Range = parameter.Range,
+            Synced = parameter.Synced,
+            Value = parameter.Value switch
+            {
+                float x => x,
+                int x => x,
+                bool x => x ? 1 : 0,
+                _ => default,
+            }
+        };
+
+        return result;
+    }
+}
