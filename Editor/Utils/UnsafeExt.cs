c46576e3dde5a8fa7720b094a562df71a0ca0f84
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-08-18 21:54:30 +0900

    feat: Add UnsafeExt

---
+++ Editor/Utils/UnsafeExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace io.github.azukimochi;
+
+internal static class UnsafeExt
+{
+    public static Span<T> AsSpan<T>(this List<T> list)
+    {
+        var tuple = Unsafe.As<Tuple<T[], int>>(list);
+        return tuple.Item1.AsSpan(0, tuple.Item2);
+    }
+}
