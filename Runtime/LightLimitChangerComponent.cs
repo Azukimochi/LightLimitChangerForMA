02c41da22cba78daa898286d5d6e7356bd58657b
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-08-18 16:36:42 +0900

    fix: MonoBehavior can not use file-scoped namespace

--- Runtime/LightLimitChangerComponent.cs
+++ Runtime/LightLimitChangerComponent.cs
@@ -1,38 +1,40 @@
 ﻿using System.Collections.Generic;
 using VRC.SDKBase;
 
-namespace io.github.azukimochi;
-
-[DisallowMultipleComponent]
-[AddComponentMenu("NDMF/Light Limit Changer")]
-public sealed class LightLimitChangerComponent : MonoBehaviour, IEditorOnly
+namespace io.github.azukimochi
 {
-    // コンポーネント作成時のバージョン マイグレーションとかに使える？
-    [HideInInspector]
-    public float Version = 2;
 
-    /// <summary>
-    /// 基本設定
-    /// </summary>
-    public GeneralSettings General;
+    [DisallowMultipleComponent]
+    [AddComponentMenu("NDMF/Light Limit Changer")]
+    public sealed class LightLimitChangerComponent : MonoBehaviour, IEditorOnly
+    {
+        // コンポーネント作成時のバージョン マイグレーションとかに使える？
+        [HideInInspector]
+        public float Version = 2;
 
-    // シェーダー固有の設定たち
-    public LilToonSettings LilToon;
-    public PoiyomiSettings Poiyomi;
-    public UnlitWFSettings UnlitWF;
+        /// <summary>
+        /// 基本設定
+        /// </summary>
+        public GeneralSettings General;
 
-    /// <summary>
-    /// 除外設定
-    /// </summary>
-    public List<GameObject> Excludes = new();
+        // シェーダー固有の設定たち
+        public LilToonSettings LilToon;
+        public PoiyomiSettings Poiyomi;
+        public UnlitWFSettings UnlitWF;
 
-    /// <summary>
-    /// WDの設定
-    /// </summary>
-    public WriteDefaultsSetting WriteDefaults = WriteDefaultsSetting.MatchAvatar;
+        /// <summary>
+        /// 除外設定
+        /// </summary>
+        public List<GameObject> Excludes = new();
 
-    /// <summary>
-    /// 対象シェーダー
-    /// </summary>
-    public SupportedShaders TargetShader = (SupportedShaders)(-1);
+        /// <summary>
+        /// WDの設定
+        /// </summary>
+        public WriteDefaultsSetting WriteDefaults = WriteDefaultsSetting.MatchAvatar;
+
+        /// <summary>
+        /// 対象シェーダー
+        /// </summary>
+        public SupportedShaders TargetShader = (SupportedShaders)(-1);
+    }
 }
