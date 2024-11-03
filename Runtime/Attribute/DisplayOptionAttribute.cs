02a82ee48efb262f9fee6868030ee4688184c341
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-11-03 21:24:30 +0900

    187_UI - 属性追加

---
+++ Runtime/Attribute/DisplayOptionAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace io.github.azukimochi;
+
+[AttributeUsage(AttributeTargets.Field)]
+internal sealed class DisplayOptionAttribute : Attribute
+{ 
+    public string Label { get; set; }
+    public string Description { get; set; }
+}
---
+++ Runtime/Attribute/SettingOptionsAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace io.github.azukimochi;
+
+[AttributeUsage(AttributeTargets.Class)]
+internal sealed class SettingOptionsAttribute : Attribute
+{
+    public SettingOptionsAttribute(string id, string displayName, string parameterPrefix = null)
+    {
+        Id = id;
+        DisplayName = displayName;
+        ParameterPrefix = parameterPrefix;
+    }
+
+    /// <summary>
+    /// ローカライズとかに使うID
+    /// </summary>
+    public string Id { get; }
+
+    /// <summary>
+    /// メニューの表示名
+    /// </summary>
+    public string DisplayName { get; }
+
+    /// <summary>
+    /// パラメーター名の接頭辞
+    /// </summary>
+    public string ParameterPrefix { get; }
+}
